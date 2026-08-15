4fc0791aae7b62fa9bf55abd21ade72b768c96ed
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-08-15 16:46:45 +0300

    feat : implement course model entity

---
+++ src/main/java/com/hei/app/model/Course.java
@@ -0,0 +1,37 @@
+package com.hei.app.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "courses", 
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_course_ref", columnNames = "ref")
+    }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String ref;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Integer credits;
+}
