561ca61c415ba795e6953553f6e2146bc696801d
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-08-15 16:21:14 +0300

    chore : format userAccount class

--- src/main/java/com/hei/app/model/UserAccount.java
+++ src/main/java/com/hei/app/model/UserAccount.java
@@ -1,7 +1,5 @@
 package com.hei.app.model;
 
-import java.util.UUID;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -10,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -18,9 +17,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "user_accounts", uniqueConstraints = {
-    @UniqueConstraint(name = "uk_user_account_email", columnNames = "email")
-})
+@Table(
+    name = "user_accounts",
+    uniqueConstraints = {@UniqueConstraint(name = "uk_user_account_email", columnNames = "email")})
 @Getter
 @Setter
 @AllArgsConstructor
@@ -28,20 +27,18 @@ import lombok.Setter;
 @Builder
 @EqualsAndHashCode
 public class UserAccount {
-    @Id
-    @GeneratedValue
-    private UUID id;
+  @Id @GeneratedValue private UUID id;
 
-    @Column( nullable = false, unique = true)
-    private String email;
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    @Column(nullable = false)
-    private String passwordHash;
+  @Column(nullable = false)
+  private String passwordHash;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Role role;
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private Role role;
 
-    @Column(nullable = false)
-    private boolean isActive;
+  @Column(nullable = false)
+  private boolean isActive;
 }
