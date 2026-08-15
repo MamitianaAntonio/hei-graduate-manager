025b1ca48e37d05cf306d6066687f281be860df8
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-08-15 16:30:58 +0300

    feat : write Teacher model class and join it with userAccount

--- src/main/java/com/hei/app/model/Teacher.java
+++ src/main/java/com/hei/app/model/Teacher.java
@@ -6,10 +6,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,30 +17,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(
-    name = "students",
-    uniqueConstraints = {@UniqueConstraint(name = "uk_student_std", columnNames = "std")})
+@Table(name = "teachers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
 @Setter
-@AllArgsConstructor
-@Builder
-@NoArgsConstructor
 @EqualsAndHashCode
-public class Student {
+public class Teacher {
   @Id @GeneratedValue private UUID id;
-
-  @Column(nullable = false, unique = true)
-  private String std;
 
   @Column(nullable = false)
   private String firstName;
 
   @Column(nullable = false)
   private String lastName;
-
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "promotion_id", nullable = false)
-  private Promotion promotion;
 
   @OneToOne(fetch = FetchType.LAZY, optional = false)
   @JoinColumn(name = "user_account_id", nullable = false, unique = true)
