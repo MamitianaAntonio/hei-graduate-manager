27d0d713f33f1fb75e6c15e7e8183c1c5fd42985
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-08-16 12:02:54 +0300

    feat : write course assignment repository with 3 specifics methods to find it

---
+++ src/main/java/com/hei/app/repository/CourseAssignmentRepository.java
@@ -0,0 +1,16 @@
+package com.hei.app.repository;
+
+import com.hei.app.model.CourseAssignment;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseAssignmentRepository extends JpaRepository<CourseAssignment, UUID> {
+  List<CourseAssignment> findByTeacherId(UUID teacherId);
+
+  List<CourseAssignment> findByCourseId(UUID courseId);
+
+  List<CourseAssignment> findByGroupId(UUID groupId);
+
+  boolean existsByTeacherIdAndCourseId(UUID teacherId, UUID courseId);
+}
