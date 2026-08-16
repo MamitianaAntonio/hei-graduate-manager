f5036249ab5400798192f59fac4ea7b87971fbc2
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-08-16 11:59:00 +0300

    feat : write course repository interface with findByRef method

---
+++ src/main/java/com/hei/app/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.hei.app.repository;
+
+import com.hei.app.model.Course;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, UUID> {
+  Optional<Course> findByRef(String ref);
+
+  boolean existsByref(String ref);
+}
