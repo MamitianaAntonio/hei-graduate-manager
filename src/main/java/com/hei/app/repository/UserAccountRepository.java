e4846eb5d7667fbbe8833706efd9d0cbaf28d202
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-08-16 11:45:17 +0300

    feat : write userAccountRepository with 2 specifics methods

---
+++ src/main/java/com/hei/app/repository/UserAccountRepository.java
@@ -0,0 +1,11 @@
+package com.hei.app.repository;
+
+import com.hei.app.model.UserAccount;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, UUID> {
+  Optional<UserAccount> findByEmail(String email);
+  boolean existsByEmail(String email);
+}
