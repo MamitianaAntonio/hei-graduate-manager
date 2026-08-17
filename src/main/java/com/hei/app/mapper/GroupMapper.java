94f2a745ad06bcf7acaea4a3cd14f2c8c1553d2d
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-08-17 17:13:58 +0300

    feat : write group mapper

---
+++ src/main/java/com/hei/app/mapper/GroupMapper.java
@@ -0,0 +1,20 @@
+package com.hei.app.mapper;
+
+import com.hei.app.dto.group.GroupRequest;
+import com.hei.app.dto.group.GroupResponse;
+import com.hei.app.model.Group;
+
+public class GroupMapper {
+  public Group toEntity(GroupRequest request) {
+    Group group = new Group();
+
+    group.setRef(request.ref());
+    group.setTrack(request.track());
+
+    return group;
+  }
+
+  public GroupResponse toResponse(Group group) {
+    return new GroupResponse(group.getId(), group.getRef(), group.getTrack());
+  }
+}
